a09da287af0fa0fddbe54a282b483c377e08e5bc
Steven Pollack <steven@gnobel.com>
2016-03-06 17:12:23 +0100

    added argparse functionality for CLI

--- organize_meetings.py
+++ organize_meetings.py
@@ -2,6 +2,7 @@ import re
 from meeting import Meeting
 from meetingroom import MeetingRoom
 from random import shuffle
+import argparse
 
 def parse_input(input_file='test_input.txt'):    
     meetings = []
@@ -33,7 +34,7 @@ def set_agenda(meetings, room1, room2):
 def organize_meetings(meetings_file='test_input.txt',
                       max_meeting_time=7*60*2,
                       max_attempts=100,
-                      verbose=True):
+                      verbose=False):
 
     meetings = parse_input(meetings_file)
 
@@ -52,22 +53,40 @@ def organize_meetings(meetings_file='test_input.txt',
     room1 = MeetingRoom("Room 1")
     room2 = MeetingRoom("Room 2")
 
+    solution_found=False
     for attempt in range(max_attempts):
-        if set_agenda(meetings, room1, room2):
+        solution_found = set_agenda(meetings, room1, room2)
+        print(attempt, solution_found)
+        if solution_found:
             break
         else:
+            print(room1)
+            print(room2)
             reset_and_shuffle(meetings)
-            set_agenda(meetings, room1, room2)
 
-    if attempt > max_attempts:
-        raise Exception("Couldn't find solution")
+    if solution_found:
+        print("Couldn't find solution within %i attempts." % max_attempts)
+        return(None)
     
     if verbose:
-        print("Found solution after %d attempts:\n" % attempt)
-        print(room1, "\n")
-        print(room2)
-        
-    return(room1, room2)
+        print("Found solution after %s attempts:\n" % str(attempt+1))
 
+    print(room1, "\n")
+    print(room2)
+        
 if __name__ == "__main__":
-    organize_meetings()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("meeting_file", 
+                        help="the path to the file containing meeting names and lengths.")
+    parser.add_argument("-m", "--max-attempts", 
+                        default=100,
+                        type=int,
+                        help="the maximum number of attempts to be performed. Default is 100.")
+    parser.add_argument("-v", "--verbose",
+                        help="print extra information about meetings and scheduling attempts.",
+                        action="store_true")
+    args = parser.parse_args()
+
+    organize_meetings(meetings_file=args.meeting_file, 
+                      max_attempts=args.max_attempts,
+                      verbose=args.verbose)
